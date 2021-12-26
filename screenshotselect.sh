3545514c1fda8324cea261bdf1f08f25664fe384
SRAZKVT <sarah.mathey66666@gmail.com>
2021-12-26 16:42:57 +0100

    fixed screenshotselect capturing selection box

--- screenshotselect.sh
+++ screenshotselect.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-scrot -s -q 100 unknown.png ; xclip -selection c -t image/png < unknown.png ; rm unknown.png
+scrot -a $(slop -f '%x,%y,%w,%h') -q 100 unknown.png ; xclip -selection c -t image/png < unknown.png ; rm unknown.png
