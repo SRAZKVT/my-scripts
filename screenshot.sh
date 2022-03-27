48cc3daf3d5863ad59fcd0e5b72297036cb2a752
SRAZKVT <sarah.mathey66666@gmail.com>
2022-03-27 09:19:28 +0200

    working towards cross x11/wayland script for screenshot

--- screenshot.sh
+++ screenshot.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-scrot -q 100 unknown.png ; xclip -selection c -t image/png < unknown.png ; rm unknown.png
+if [ $XDG_SESSION_TYPE = 'x11' ]; then
+	scrot -q 100 unknown.png
+	xclip -selection c -t image/png < unknown.png
+elif [ $XDG_SESSION_TYPE = 'wayland' ]; then
+	### TODO: use screenshot utility working under wayland
+	printf "this script yet isn't supporting wayland\n"
+	exit 1
+	wl-copy < unknown.png
+fi
+rm unknown.png
