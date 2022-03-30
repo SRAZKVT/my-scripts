4ae034b890854ac650a9e911227b86c6ad006aee
SRAZKVT <sarah.mathey66666@gmail.com>
2022-03-30 20:34:19 +0200

    improved slightly asm compiler

--- asmcompile.sh
+++ asmcompile.sh
@@ -3,29 +3,33 @@ auto_run_flag=0
 
 
 while getopts 'cr' FLAG
-	do
-    	case $FLAG in
-        	c)	cleanup_flag=1
-        		;;
-    		r)	auto_run_flag=1
-    			;;
-			?)	printf "Usage: $0 [-c] [-r] [FILENAME]\n"
-				exit 1
-				;;
-    	esac
-	done
+    do
+       case $FLAG in
+           c)     cleanup_flag=1
+                  ;;
+           r)     auto_run_flag=1
+                  ;;
+           ?)     printf "Usage: $0 [-c] [-r] [FILENAME]\n"
+                  exit 1
+                  ;;
+       esac
+    done
 shift $(($OPTIND - 1))
 
-
+extension=$(echo $1 |awk -F . '{if (NF>1) {print $NF}}')
 argcount=$#
-filename=$(echo "$1" | cut -f 1 -d '.')
+if [ ${extension} != "asm" ]; then
+    printf "Selected file should have a .asm extension\n"
+    exit 1
+fi
+filename=$(echo $(realpath $1) | cut -f 1 -d '.')
 
 if [ ${argcount} -eq 1 ]; then
     printf "Compiling...\n"
     nasm -f elf64 -g -F DWARF ${filename}.asm
     printf "Object file creation complete, linking...\n"
     ld -e start -o ${filename} ${filename}.o
-    printf "Finished linking, run ./${filename} to see the output\n"
+    printf "Finished linking, run $(realpath $filename) to see the output\n"
 elif [ ${argcount} -gt 1 ]; then
     printf "Please input path to only one file\n"
     exit 1
@@ -34,11 +38,11 @@ else
     exit 1
 fi
 if [ ${cleanup_flag} -eq 1 ]; then
-	printf "Removing object file\n"
-	rm ${filename}.o
+     printf "Removing object file\n"
+     rm ${filename}.o
 fi
 if [ ${auto_run_flag} -eq 1 ]; then
-	printf "Running the generated file ...\n\n"
-	$(realpath ${filename})
+     printf "Running the generated file ...\n\n"
+     $(realpath ${filename})
 fi
 exit 0
